dcb45f74069c4c8deadcf11e736673159a78dc3a
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-11 10:49:32 +0500

    s

--- ArmstrongNum.py
+++ ArmstrongNum.py
@@ -2,6 +2,8 @@
 n=int(input("Enter any number: "))
 a=list(map(int,str(n)))
 b=list(map(lambda x:x**3,a))
+
+# Using if statement.
 if(sum(b)==n):
     print("The number is an armstrong number. ")
 else:
